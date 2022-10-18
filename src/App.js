919ab93c36a437af542228e20f5011e6f3b3c63c
MisratIra <misratmehenaj199@gmail.com>
2022-10-18 06:39:24 +0600

    updated home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
+	return (
+		<div className="App bg-slate-200">
+			<Header></Header>
+			<Routes>
+				<Route path="/home" element={<Home></Home>}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div className="flex items-center  mx-auto w-11/12">
+			<div className="w-full">
+				<h2 className="text-2xl">This is Product review page</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut placeat
+					sunt totam beatae esse non, voluptate veritatis dolor nesciunt alias
+					nulla blanditiis optio dolores quis sapiente? In assumenda sit
+					inventore illo commodi neque et similique pariatur. Autem ipsam
+					deserunt sapiente.
+				</p>
+			</div>
+			<div className="">
+				<img className="" src="laptop.png" alt="" />
+			</div>
+		</div>
+	);
+};
+
+export default Home;
