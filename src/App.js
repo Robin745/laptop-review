ff5d4b40a1a4a3cc4852520618783b30a1d074f3
Robin745 <moshiur251@gmail.com>
2022-10-18 15:51:35 +0600

    home layout updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
 function App() {
-	return (
-		<div className="App bg-slate-200">
-			<Header></Header>
-			<Routes>
-				<Route path="/home" element={<Home></Home>}></Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App bg-slate-200">
+      <Header></Header>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,21 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div className="flex items-center  mx-auto w-11/12">
-			<div className="w-full">
-				<h2 className="text-2xl">This is Product review page</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut placeat
-					sunt totam beatae esse non, voluptate veritatis dolor nesciunt alias
-					nulla blanditiis optio dolores quis sapiente? In assumenda sit
-					inventore illo commodi neque et similique pariatur. Autem ipsam
-					deserunt sapiente.
+		<div className="flex flex-col md:flex md:flex-row items-center my-8 md:my-0 mx-auto w-11/12">
+			<div className="w-full text-left p-4 pb-0 order-2 md:order-1">
+				<h2 className="text-3xl mb-4">HP 11th Gen Intel Core i5 IPS 14 Inch.</h2>
+				<p className="">
+					Laptop Brand - HP, Laptop Model - HP 14s-dq5345TU, Processor Brand - Intel, Processor Type
+					- Intel Core i3, Processor Generation - 12th (Intel), Processor Model - Core i3 1215U,
+					Processor Max Turbo Frequency - 4.40 GHz, Processor Core - 6, Processor Thread - 8,
+					Processor Cache - 10MB, Chipset - Intel Integrated SoC, Display Size - 14 Inch,
+					Display Technology - FHD IPS LED Display, Display Resolution - 1920 x 1080.
+				</p>
+				<p className="w-40 my-3 py-2 text-center bg-cyan-600 hover:bg-purple-900 rounded text-white text-xl">
+					<button>Review</button>
 				</p>
 			</div>
-			<div className="">
+			<div className="order-1 md:order-2">
 				<img className="" src="laptop.png" alt="" />
 			</div>
 		</div>
