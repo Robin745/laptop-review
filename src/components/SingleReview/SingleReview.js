e5b66d3389379e5ddd43ba88d34b986be21d2b10
Robin745 <moshiur251@gmail.com>
2022-10-19 02:15:08 +0600

    ratings added

--- src/components/SingleReview/SingleReview.js
+++ src/components/SingleReview/SingleReview.js
@@ -1,14 +1,28 @@
 import React from "react";
-
+import { StarIcon } from '@heroicons/react/24/solid';
 const SingleReview = ({ review }) => {
-	const { name, comment, picture } = review;
+	const { name, comment, picture, rating } = review;
 	return (
-		<div className="w-10/12 mx-auto">
-			<div className="w-20 ">
-				<img className="w-full rounded-full" src={picture} alt="" />
-				<h4>{name}</h4>
+		<div className="w-10/12 mx-auto bg-white my-3 p-4">
+			<div className="flex my-1">
+				<StarIcon className="w-6 text-gray-300"></StarIcon>
+				<StarIcon className="w-6 text-gray-300"></StarIcon>
+				<StarIcon className="w-6 text-gray-300"></StarIcon>
+				<StarIcon className="w-6 text-gray-300"></StarIcon>
+				<StarIcon className="w-6 text-gray-300"></StarIcon>
 			</div>
-			<p> {comment}</p>
+			<div className="text-left flex">
+				{rating}<StarIcon className="w-6 text-cyan-800"></StarIcon>
+				<p className="ml-1">Ratings </p>
+			</div>
+			<div className=" flex items-center">
+				<img className="w-12 rounded-full" src={picture} alt="" />
+				<h4 className="ml-4 text-xl">{name}</h4>
+			</div>
+
+			<div className="text-left">
+				<p> {comment}</p>
+			</div>
 		</div>
 	);
 };
